626e0a91feba8bbaade3ce586ba5e68509e3a3dd
Ci Zhang <cz2397@holmes.cm.cluster>
2017-06-13 16:41:12 -0400

    requested changes

--- floater/utils.py
+++ floater/utils.py
@@ -251,14 +251,16 @@ def floats_to_netcdf(input_dir, output_fname,
 
     match_pattern = float_file_prefix + '.*.csv'
     float_files = glob(os.path.join(input_dir, match_pattern))
+    header_type = type(dd.read_csv(float_files[0]).columns[0])
     float_timesteps = sorted(list({int(float_file[-22:-12]) for float_file in float_files}))
     float_columns = ['npart', 'time', 'x', 'y', 'z', 'u', 'v', 'vort']
 
     for float_timestep in tqdm(float_timesteps):
         input_path = os.path.join(input_dir, '%s.%010d.*.csv' % (float_file_prefix, float_timestep))
-        df = dd.read_csv(input_path)
-        if df.columns.values[0] is not 'npart':
+        if header_type is not str:
             df = dd.read_csv(input_path, names=float_columns, header=None)
+        else:
+            df = dd.read_csv(input_path)
         dfc = df.compute()
         dfcs = dfc.sort_values('npart')
         del_time = int(dfcs.time.values[0])
@@ -270,7 +272,7 @@ def floats_to_netcdf(input_dir, output_fname,
             time = np.array([np.int32(del_time)])
         npart = dfcs.npart.values.astype(np.int32)
         var_shape = (1, len(npart))
-        var_names = dfcs.columns.values[2:]
+        var_names = list(dfcs.columns)[2:]
         data_vars = {var_name: (['time', 'npart'], dfcs[var_name].values.astype(np.float32).reshape(var_shape)) for var_name in var_names}
         ds = xr.Dataset(data_vars, coords={'time': time, 'npart': npart})
         if pkl_path is not None:
